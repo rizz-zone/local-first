8776ddd737070656e096ef4d510b9426a78d1958
ezShroom <68300730+ezShroom@users.noreply.github.com>
2025-06-24 18:21:22 +0100

    add first events and context

--- src/machines/worker.test.ts
+++ src/machines/worker.test.ts
@@ -1,3 +1,16 @@
-import { describe } from 'vitest'
+import { describe, expect, it } from 'vitest'
+import { clientMachine } from './worker'
+import { createActor } from 'xstate'
 
-describe('worker machine', () => {})
+describe('worker machine', () => {
+	it('starts with no connections', () => {
+		const machine = createActor(clientMachine)
+		machine.start()
+
+		const snapshot = machine.getSnapshot()
+		expect(snapshot.value).toEqual({
+			websocket: 'disconnected',
+			db: 'disconnected'
+		})
+	})
+})
--- src/machines/worker.ts
+++ src/machines/worker.ts
@@ -1,16 +1,63 @@
-import { createMachine } from 'xstate'
+import { assign, setup } from 'xstate'
 
-export const clientMachine = createMachine({
+export const clientMachine = setup({
+	types: {
+		context: {} as {
+			socket?: WebSocket
+			wsUrl?: string
+			dbName?: string
+		},
+		events: {} as
+			| { type: 'init'; wsUrl: string; dbName: string }
+			| { type: 'connected' }
+	},
+	actions: {
+		establishSocket: assign(({ context, self }) => {
+			if (!context.wsUrl) return {}
+			const socket = new WebSocket(context.wsUrl)
+			socket.onopen = () => self.send({ type: 'connected' })
+			return { socket }
+		}),
+		establishDb: assign(() => ({})),
+		initWsUrl: assign(({ event }) => {
+			if (event.type !== 'init') return {}
+			return { wsUrl: event.wsUrl }
+		}),
+		initDbName: assign(({ event }) => {
+			if (event.type !== 'init') return {}
+			return { dbName: `${event.dbName}.sqlite` }
+		})
+	}
+}).createMachine({
 	type: 'parallel',
 	states: {
 		websocket: {
 			initial: 'disconnected',
-			states: { connected: {}, disconnected: {} }
+			states: {
+				connected: {},
+				disconnected: {
+					entry: 'establishSocket',
+					on: {
+						init: {
+							actions: ['initWsUrl', 'establishSocket']
+						},
+						connected: {
+							target: 'connected'
+						}
+					}
+				}
+			}
 		},
 		db: {
 			initial: 'disconnected',
 			states: {
-				disconnected: {},
+				disconnected: {
+					on: {
+						init: {
+							actions: ['initDbName']
+						}
+					}
+				},
 				'will never connect': {
 					type: 'final'
 				},
