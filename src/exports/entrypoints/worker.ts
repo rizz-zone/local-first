7e6d69853f5519ed932588a806331af7eb52367e
ezShroom <68300730+ezShroom@users.noreply.github.com>
2025-06-28 08:22:59 +0100

    did some more on the normal worker

--- src/exports/entrypoints/worker.ts
+++ src/exports/entrypoints/worker.ts
@@ -1,7 +1,24 @@
 /// <reference lib="webworker" />
 
+import { WorkerLocalFirst } from '../../classes/worker_thread'
+import {
+	UpstreamWorkerMessageType,
+	type UpstreamWorkerMessage
+} from '../../types/messages/worker/UpstreamWorkerMessage'
 import type { Transition } from '../../types/transitions/Transition'
 
 export function workerEntrypoint<TransitionSchema extends Transition>() {
-	onmessage = (event: MessageEvent<TransitionSchema>) => {}
+	const ourObject = new WorkerLocalFirst()
+
+	const onmessage = (
+		event: MessageEvent<UpstreamWorkerMessage<TransitionSchema>>
+	) => {
+		const message = event.data
+		switch (message.type) {
+			case UpstreamWorkerMessageType.Init: {
+				const { wsUrl, dbName } = message.data
+				ourObject.init()
+			}
+		}
+	}
 }
