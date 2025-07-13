d76e2e52fdde2c3ceac9570790f7fa41788f6913
ezShroom <68300730+ezShroom@users.noreply.github.com>
2025-07-13 16:11:28 +0100

    test normal cases

--- src/impl/browser/helpers/port_manager.test.ts
+++ src/impl/browser/helpers/port_manager.test.ts
@@ -23,6 +23,7 @@ import { DB_NAME, SOCKET_URL } from '../../../testing/constants'
 import { importUnique } from '../../../testing/dynamic_import'
 import type { InstanceKey } from '../../../types/common/client/InstanceKey'
 import type { WorkerLocalFirst } from './worker_thread'
+import type { InstanceData } from '../../../types/common/client/InstanceData'
 
 const FORBIDDEN_WORKER_PORT =
 	'__testing__do_not_use_this_ever_or_you_will_have_a_terrible_time_and_also_cause_probably_pretty_major_and_significant_bugs_and_we_wouldnt_want_that_would_we__WorkerPort'
@@ -99,7 +100,7 @@ describe('port message listener', ({ skip }) => {
 		userPort = channel.port2
 	})
 
-	describe('on main thread init', () => {
+	describe('main thread init', () => {
 		const classInstanceMap =
 			// @ts-expect-error instances is private, but in this case it's convenient to see it anyway
 			forbidden_WorkerPort.instances
@@ -112,13 +113,13 @@ describe('port message listener', ({ skip }) => {
 			})
 		})
 
-		test('assigns the correct static properties', async () => {
+		test('assigns the correct static properties', () => {
 			const addedInstance = classInstanceMap.get(`${SOCKET_URL}::${DB_NAME}`)
 			expect(classInstanceMap.size).toBe(1)
 			expect(addedInstance).toBeDefined()
 			expect(addedInstance).toBeTypeOf('object')
 		})
-		test('sets a ping timeout', async () => {
+		test('sets a ping timeout', () => {
 			expect(clearTimeoutMock).not.toBeCalled()
 			expect(setTimeoutMock).toHaveBeenCalledOnce()
 		})
@@ -151,55 +152,155 @@ describe('port message listener', ({ skip }) => {
 describe('Symbol.dispose', () => {
 	let instances: Map<InstanceKey, WorkerLocalFirst>
 	let activeInstanceClients: Map<InstanceKey, number>
-	let init: () => unknown
 	beforeEach(async () => {
 		const uniqueInstance: {
 			[FORBIDDEN_WORKER_PORT]: typeof forbidden_WorkerPort
 			portManager: typeof portManager
 		} = await importUnique('./port_manager')
-		init = uniqueInstance.portManager.init
 		instances =
 			// @ts-expect-error instances is private, but in this case it's convenient to see it anyway
 			uniqueInstance[FORBIDDEN_WORKER_PORT].instances
 		activeInstanceClients =
 			// @ts-expect-error instances is private, but in this case it's convenient to see it anyway
 			uniqueInstance[FORBIDDEN_WORKER_PORT].activeInstanceClients
+
+		uniqueInstance.portManager.init()
 	})
-	it('does not modify static properties if init never happened', ({ skip }) => {
-		const instancesSetMock = vi.spyOn(instances, 'set')
-		const activeInstanceClientsSetMock = vi.spyOn(activeInstanceClients, 'set')
+	describe('unusual cases', () => {
+		it('does not modify static properties if init never happened', ({
+			skip
+		}) => {
+			const instancesSetMock = vi.spyOn(instances, 'set')
+			const activeInstanceClientsSetMock = vi.spyOn(
+				activeInstanceClients,
+				'set'
+			)
 
-		init()
-		if (!ctx.onconnect) return skip()
-		ctx.onconnect(
-			new MessageEvent('connect', { ports: [new MessageChannel().port1] })
-		)
+			if (!ctx.onconnect) return skip()
+			ctx.onconnect(
+				new MessageEvent('connect', { ports: [new MessageChannel().port1] })
+			)
 
-		if (!setTimeoutMock.mock.lastCall) return skip("setTimeout wasn't called")
-		if (typeof setTimeoutMock.mock.lastCall[0] !== 'function')
-			return skip('setTimeout not called with function')
+			if (!setTimeoutMock.mock.lastCall) return skip("setTimeout wasn't called")
+			if (typeof setTimeoutMock.mock.lastCall[0] !== 'function')
+				return skip('setTimeout not called with function')
 
-		setTimeoutMock.mock.lastCall[0]()
-		expect(instancesSetMock).not.toBeCalled()
-		expect(activeInstanceClientsSetMock).not.toBeCalled()
+			setTimeoutMock.mock.lastCall[0]()
+			expect(instancesSetMock).not.toBeCalled()
+			expect(activeInstanceClientsSetMock).not.toBeCalled()
+		})
+		it('throws error on inconsistency with activeInstanceClients', ({
+			skip
+		}) => {
+			if (!ctx.onconnect) return skip("ctx.onconnect wasn't set")
+			const port = new MessageChannel().port1
+			ctx.onconnect(new MessageEvent('connect', { ports: [port] }))
+			if (!port.onmessage) return skip("port.onmessage wasn't set")
+			port.onmessage(
+				new MessageEvent<UpstreamWorkerMessage<unknown>>('message', {
+					data: NORMAL_INIT_WORKER_MESSAGE
+				})
+			)
+
+			if (!setTimeoutMock.mock.lastCall) return skip("setTimeout wasn't called")
+			if (typeof setTimeoutMock.mock.lastCall[0] !== 'function')
+				return skip('setTimeout not called with function')
+
+			expect(activeInstanceClients.delete(`${SOCKET_URL}::${DB_NAME}`)).toBe(
+				true
+			)
+			expect(setTimeoutMock.mock.lastCall[0]).toThrow(InternalStateError)
+		})
 	})
-	it('throws error on inconsistency with activeInstanceClients', ({ skip }) => {
-		init()
-		if (!ctx.onconnect) return skip("ctx.onconnect wasn't set")
-		const port = new MessageChannel().port1
-		ctx.onconnect(new MessageEvent('connect', { ports: [port] }))
-		if (!port.onmessage) return skip("port.onmessage wasn't set")
-		port.onmessage(
-			new MessageEvent<UpstreamWorkerMessage<unknown>>('message', {
-				data: NORMAL_INIT_WORKER_MESSAGE
-			})
-		)
+	describe('normal cases', () => {
+		function createAndInit(data: InstanceData): boolean {
+			if (!ctx.onconnect) return false
+			const port = new MessageChannel().port1
+			ctx.onconnect(new MessageEvent('connect', { ports: [port] }))
+			if (!port.onmessage) return false
+			port.onmessage(
+				new MessageEvent<UpstreamWorkerMessage<unknown>>('message', {
+					data: {
+						type: UpstreamWorkerMessageType.Init,
+						data
+					}
+				})
+			)
 
-		if (!setTimeoutMock.mock.lastCall) return skip("setTimeout wasn't called")
-		if (typeof setTimeoutMock.mock.lastCall[0] !== 'function')
-			return skip('setTimeout not called with function')
+			return Boolean(setTimeoutMock.mock.lastCall)
+		}
 
-		expect(activeInstanceClients.delete(`${SOCKET_URL}::${DB_NAME}`)).toBe(true)
-		expect(setTimeoutMock.mock.lastCall[0]).toThrow(InternalStateError)
+		it('removes the correct instance with clients: (1) this instance', ({
+			skip
+		}) => {
+			if (
+				!createAndInit({
+					wsUrl: SOCKET_URL,
+					dbName: DB_NAME
+				})
+			)
+				return skip('failed to createAndInit')
+			const call = setTimeoutMock.mock.lastCall
+			if (!call) return skip('no lastCall')
+			const symbolDestroy = call[0]
+
+			symbolDestroy()
+
+			expect(instances.size).toBe(0)
+			expect(activeInstanceClients.size).toBe(0)
+		})
+		it('removes the correct instance with clients: (1) this instance, (1) another instance', ({
+			skip
+		}) => {
+			for (let i = 0; i <= 1; i++) {
+				if (
+					!createAndInit({
+						wsUrl: SOCKET_URL,
+						// The final one needs to be the one we're deleting
+						dbName: i === 0 ? DB_NAME + 'Plus' : DB_NAME
+					})
+				)
+					return skip('failed to createAndInit')
+			}
+			const call = setTimeoutMock.mock.lastCall
+			if (!call) return skip('no lastCall')
+			const symbolDestroy = call[0]
+
+			symbolDestroy()
+
+			expect(instances.size).toBe(1)
+			expect(instances.get(`${SOCKET_URL}::${DB_NAME}`)).toBeUndefined()
+			expect(instances.get(`${SOCKET_URL}::${DB_NAME}Plus`)).toBeTypeOf(
+				'object'
+			)
+			expect(activeInstanceClients.size).toBe(1)
+			expect(
+				activeInstanceClients.get(`${SOCKET_URL}::${DB_NAME}`)
+			).toBeUndefined()
+			expect(activeInstanceClients.get(`${SOCKET_URL}::${DB_NAME}Plus`)).toBe(1)
+		})
+		it('does not remove any instances with clients: (2) this instance', ({
+			skip
+		}) => {
+			for (let i = 0; i <= 1; i++) {
+				if (
+					!createAndInit({
+						wsUrl: SOCKET_URL,
+						dbName: DB_NAME
+					})
+				)
+					return skip('failed to createAndInit')
+			}
+			const call = setTimeoutMock.mock.lastCall
+			if (!call) return skip('no lastCall')
+			const symbolDestroy = call[0]
+
+			symbolDestroy()
+
+			expect(instances.size).toBe(1)
+			expect(instances.get(`${SOCKET_URL}::${DB_NAME}`)).toBeTypeOf('object')
+			expect(activeInstanceClients.size).toBe(1)
+			expect(activeInstanceClients.get(`${SOCKET_URL}::${DB_NAME}`)).toBe(1)
+		})
 	})
 })
