4c77022bab1987eb9cbe044d5e484e2552c03d63
ezShroom <68300730+ezShroom@users.noreply.github.com>
2025-07-14 14:25:54 +0100

    expand the DO. this is roughly the point at which we need to expand into a monorepo

--- src/impl/do/exports/durable_object.ts
+++ src/impl/do/exports/durable_object.ts
@@ -4,14 +4,53 @@ import { DurableObject } from 'cloudflare:workers'
 import type { Transition } from '../../../types/transitions/Transition'
 import type { SyncEngineDefinition } from '../../shared/exports/engine'
 
-export function createDurableObject<T extends Transition>(
-	engineDef: SyncEngineDefinition<T>
-) {
-	// 'Anonymous' classes like this can't have TypeScript private properties
-	// for some reason. We have to simply accept and deal with this.
-	return class extends DurableObject {
-		constructor(ctx: DurableObjectState, env: Env) {
-			super(ctx, env)
+type CheckFetch = (request: Request) => boolean
+
+export class SyncEngineBackend<
+	T extends Transition,
+	Env extends object
+> extends DurableObject {
+	private engineDef: SyncEngineDefinition<T>
+	private checkFetch?: CheckFetch
+
+	constructor(
+		ctx: DurableObjectState,
+		env: Env,
+		options: {
+			engineDef: SyncEngineDefinition<T>
+			checkFetch?: CheckFetch
 		}
+	) {
+		super(ctx, env)
+		this.engineDef = options.engineDef
+	}
+
+	override async fetch(request: Request) {
+		this.checkFetch?.(request)
+
+		// Create two ends of a WebSocket connection
+		const webSocketPair = new WebSocketPair()
+		const [client, server] = Object.values(webSocketPair)
+		if (!server) return new Response(null, { status: 500 })
+
+		// Accept the server one
+		this.ctx.acceptWebSocket(server)
+
+		return new Response(null, {
+			status: 101,
+			webSocket: client
+		})
+	}
+
+	override async webSocketMessage(
+		ws: WebSocket,
+		message: string | ArrayBuffer
+	) {
+		// Upon receiving a message from the client, reply with the same message,
+		// but will prefix the message with "[Durable Object]: " and return the
+		// total number of connections.
+		ws.send(
+			`[Durable Object] message: ${message}, connections: ${this.ctx.getWebSockets().length}`
+		)
 	}
 }
